c0dbe60356d131580798d927d2572cd1b45cddd4
vlaship <vlaship@outlook.com>
2024-05-27 22:43:05 -0400

    fixed after review

--- src/db/migrations/20240521020202_create_users.sql
+++ src/db/migrations/20240521020202_create_users.sql
@@ -1,9 +1,8 @@
-create table book_catalog_rs.users
+create table if not exists book_catalog_rs.users
 (
     id bigint not null constraint users_pk primary key,
     login    text   not null,
     password text   not null
 );
 
-create unique index users_login_uindex on book_catalog_rs.users (login);
-
+create unique index if not exists users_login_uindex on book_catalog_rs.users (login);
